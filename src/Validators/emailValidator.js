d936af0bad699f7512292c7e4180dcb63e37241f
Marko Tintor <marko.tintor.18@singimail.rs>
2025-09-07 17:46:06 +0200

    domaci

--- src/Validators/emailValidator.js
+++ src/Validators/emailValidator.js
@@ -2,8 +2,19 @@ export const emailValidator = {
   required: true,
   validate: {
     trimCheck: (value) =>
-      value.trim().length > 0 || "Email ne sme sadržati samo razmake",
+      (value.trim().length > 0 && value.trim().length) >= 10 ||
+      "Email ne sme sadržati samo razmake i mora biti minimum 10 karaktera",
     simpleCheck: (value) =>
       (value.includes("@") && value.includes(".")) || "Unesite ispravan email",
+    adminCheck: (value) => {
+      const blocked = ["admin@gmail.com", "test@gmail.com"];
+      if (blocked.includes(value.trim())) return "Nemas pristup tom emailu";
+      return true;
+    },
+    gmailCheck: (value) => {
+      if (!value.trim().endsWith("@gmail.com"))
+        return "Email mora biti @gmail.com";
+      return true;
+    },
   },
 };
--- src/Validators/passwordValidator.js
+++ src/Validators/passwordValidator.js
@@ -2,5 +2,19 @@ export const passwordValidator = {
   required: true,
   validate: {
     trimCheck: (value) => value.trim() !== "" || "unesite ispravnu lozinku",
+    trim2Check: (value) =>
+      (value.trim().length >= 3 && value.trim().length <= 10) ||
+      "lozinka mora biti izmedju 3 i 10 karaktera",
+    specialCharacterCheck: (value) => {
+      const characters = ["!", "@", "#", "$", "&"];
+      const hasSpecial = characters.some((char) => value.includes(char));
+      if (!hasSpecial) {
+        return "Lozinka mora imati bar jedan specijalan karakter (!,@,#,$,&)";
+      }
+      return true;
+    },
+    blockedPw: (value) =>
+      (value.trim() !== "admin" && value.trim() !== "123456") ||
+      "ne moze lozinka biti admin ili 123456",
   },
 };
